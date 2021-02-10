a90c196889c4e6be1acce7788dd4b04885e1b50f
truncet <theonlyeminem@gmail.com>
2021-02-10 23:53:28 +0530

    persistance added

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store.reducer';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store.reducer';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
