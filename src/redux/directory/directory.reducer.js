3fa78b70f500e47a9581dc3787e5d5163765c643
truncet <theonlyeminem@gmail.com>
2021-05-21 13:59:37 +0545

    minor change

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,39 +1,42 @@
 const INITIAL_STATE = {
     sections: [
        {
-           title:'hats',
-           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+           title:'Home Office',
+           imageUrl: 'https://bit.ly/3fsdcdK',
            id: 1,
-           linkUrl: 'shop/hats'
+           linkUrl: 'shop/homeoffice'
        },
        {
-           title: 'jackets',
-           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+           title: 'outdoor',
+           imageUrl: 'https://bit.ly/2RsDqoo',
            id:2,
-           linkUrl: 'shop/jackets'
+           linkUrl: 'shop/outdoor'
        },
        {
-           title: 'sneakers',
-           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+           title: 'Bedroom',
+           imageUrl: 'https://bit.ly/3fuH8WH',
            id:3,
-           linkUrl: 'shop/sneakers'
+           linkUrl: 'shop/bedroom'
        },
        {
-           title: 'womens',
-           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+           title: 'Dining',
+           imageUrl: 'https://bit.ly/bangla-dining',
            id:4,
            size:'large',
-           linkUrl: 'shop/womens'
+           linkUrl: 'shop/dining'
        },
        {
-           title: 'mens',
-           imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
+           title: 'Living',
+           imageUrl: 'https://bit.ly/bangla-living',
            id:5,
            size:'large',
-           linkUrl: 'shop/mens'
+           linkUrl: 'shop/living'
        }
    ]
 }
+
+
+
 
 
 const directoryReducer = (state = INITIAL_STATE, action) =>{
