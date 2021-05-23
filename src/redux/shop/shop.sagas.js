f0d7af1ffc2f110b281685c24a09dc14d12a29d2
truncet <theonlyeminem@gmail.com>
2021-05-23 14:19:11 +0545

    sagas added

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,28 @@
+import {takeLatest , call , put} from 'redux-saga/effects';
+import ShopActionTypes from './shop.types';
+
+import {firestore, converCollectionsSnapshotToMap} from './../../firebase/firebase.utils';
+
+import {fetchCollectionFailure, fetchCollectionSuccess } from './shop.actions';
+
+
+export function* fetchCollectionsAsync(){
+   try {
+       const collectionRef = firestore.collection('collections');
+       const snapshot = yield collectionRef.get();
+       const collectionsMap = yield call (
+           converCollectionsSnapshotToMap,
+           snapshot,
+       );
+       yield put(fetchCollectionSuccess(collectionsMap));
+   } catch(error){
+       yield put(fetchCollectionFailure(error.message));
+   }
+}
+
+export function* fetchCollectionsStart(){
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionsAsync
+    );
+}
--- src/redux/store.reducer.js
+++ src/redux/store.reducer.js
@@ -1,12 +1,14 @@
 import {createStore, applyMiddleware} from 'redux';
 import {logger} from 'redux-logger';
 import {persistStore} from 'redux-persist'
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root.reducer';
+import rootSaga from './root-saga';
 
+const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger)
@@ -14,4 +16,5 @@ if(process.env.NODE_ENV === 'development'){
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
